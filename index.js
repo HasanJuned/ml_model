ffebb942e78dc27420ff4984b8c1c52eafca22bf
Hassan <hasanahmad9812021@gmail.com>
2025-06-02 00:36:36 +0600

    up

--- index.js
+++ index.js
@@ -27,5 +27,5 @@ app.post('/predict', (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+  console.log('Server running on http://localhost:8000');
 });
