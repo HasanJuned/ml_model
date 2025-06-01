d7ede17b94fa8749c0ddd1b8031b3fd4b49f5926
Hassan <hasanahmad9812021@gmail.com>
2025-06-01 22:50:16 +0600

    up

--- predict.py
+++ predict.py
@@ -1,8 +1,24 @@
 import sys
 import json
+import joblib
+import numpy as np
+
+model = joblib.load("logreg_model.pkl")
 
 try:
     input_data = json.load(sys.stdin)
-    print(json.dumps({"status": "received", "input_keys": list(input_data.keys())}))
+
+    feature_order = [
+        "age", "sex", "cp", "trestbps", "chol", "fbs",
+        "restecg", "thalach", "exang", "oldpeak", "slope", "ca", "thal"
+    ]
+
+    features = [input_data[feature] for feature in feature_order]
+    features_array = np.array(features).reshape(1, -1)
+
+    prediction = model.predict(features_array)[0]
+
+    print(json.dumps({"prediction": int(prediction)}))
+
 except Exception as e:
     print(json.dumps({"error": str(e)}))
